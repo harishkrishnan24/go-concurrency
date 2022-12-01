581a738d8c20bcd0502d1fc995c1711af7e18390
Harish Krishnan <h.krishnan@sap.com>
2022-12-01 18:36:33 +0530

    Producer consumer

---
+++ producer-consumer/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	for {
+
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
+}
