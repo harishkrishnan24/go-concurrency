05bb56aebea65336329d37795fefe03dd8768689
Harish Krishnan <h.krishnan@sap.com>
2022-12-26 17:06:06 +0530

    Create barber routine

---
+++ sleeping-barber/barbershop.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type BarberShop struct {
+	ShopCapacity    int
+	HairCutDuration time.Duration
+	NumberOfBarbers int
+	BarbersDoneChan chan bool
+	ClientsChan     chan string
+	Open            bool
+}
+
+func (shop *BarberShop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients.", barber)
+
+		for {
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up", client, barber)
+					isSleeping = false
+				}
+
+				shop.cutHair(barber, client)
+			} else {
+				shop.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (shop *BarberShop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair.", barber, client)
+	time.Sleep(shop.HairCutDuration)
+	color.Green("%s is finished cutting %s's hair", barber, client)
+}
+
+func (shop *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home", barber)
+	shop.BarbersDoneChan <- true
+}
---
+++ sleeping-barber/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
+
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
+
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
+
+	shop.addBarber("Frank")
+
+	time.Sleep(5 * time.Second)
+}
